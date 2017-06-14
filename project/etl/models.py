dd5164e6c57d9f6a6772aafa3687a66c90135999
Pandza11 <ivan.pandzic@gmail.com>
2017-06-14 10:24:23 +0200

    Updated models #2

--- project/etl/models.py
+++ project/etl/models.py
@@ -5,43 +5,6 @@ from django.utils.timezone import datetime
 
 # Create your models here.
 
-class Account(models.Model):
-    """
-    Represents a registered user.
-    """
-    email = models.EmailField(max_length=254, blank=False)
-    password = models.CharField(max_length=1024, blank=False)
-    createdAt = models.DateField(null=True, blank=True, default=None)
-    name = models.CharField(max_length=1024)
-    key = models.CharField(max_length=1024, default='uuid') # What to do with UUID?
-    date = models.DateField(max_length=1024, default=datetime.today)
-    defaultTTL = models.IntegerField(max_length=1024)
-    googleId = models.CharField(max_length=1024)
-    facebookId = models.CharField(max_length=1024)
-    twitterId = models.CharField(max_length=1024)
-    googleProfile_id = models.CharField(max_length=1024)
-    googleProfile_username = models.CharField(max_length=1024)
-    googleProfile_displayname = models.CharField(max_length=1024)
-    facebookProfile_id = models.CharField(max_length=1024)
-    facebookProfile_username = models.CharField(max_length=1024)
-    facebookProfile_displayname = models.CharField(max_length=1024)
-    twitterProfile_id = models.CharField(max_length=1024)
-    twitterProfile_username = models.CharField(max_length=1024)
-    twitterProfile_displayname = models.CharField(max_length=1024)
-    subscriptions = models.CharField(max_length=1024)
-    referral = models.CharField(max_length=1024)
-    refSrc = models.CharField(max_length=1024)
-    firstSeen = models.DateField(max_length=1024)
-    enableClassroom = models.BooleanField(default=False)
-    memberOf = models.CharField(max_length=1024)
-    isAffiliate = models.BooleanField(default=False)
-    isAWWAdmin = models.BooleanField(default=False)
-    accountType = models.CharField(max_length=1024, default='fun')
-    enableNewsletter = models.BooleanField(default=False)
-    lastLoginAt = models.DateField(max_length=1024)
-
-    def __str__(self):
-        return self.name
 
 class Subscription(models.Model):
     """
@@ -59,3 +22,42 @@ class Subscription(models.Model):
 
     def __str__(self):
         return self.couponID
+
+
+class Account(models.Model):
+    """
+    Represents a registered user.
+    """
+    email = models.EmailField(max_length=254, blank=False)
+    password = models.CharField(max_length=1024, blank=False)
+    createdAt = models.DateField(null=True, blank=True, default=None)
+    name = models.CharField(max_length=1024)
+    key = models.CharField(max_length=1024, default='uuid')# What to do with UUID?
+    date = models.DateField(max_length=1024, default=datetime.today)
+    defaultTTL = models.IntegerField(max_length=1024)
+    googleId = models.CharField(max_length=1024)
+    facebookId = models.CharField(max_length=1024)
+    twitterId = models.CharField(max_length=1024)
+    googleProfile_id = models.CharField(max_length=1024)
+    googleProfile_username = models.CharField(max_length=1024)
+    googleProfile_displayname = models.CharField(max_length=1024)
+    facebookProfile_id = models.CharField(max_length=1024)
+    facebookProfile_username = models.CharField(max_length=1024)
+    facebookProfile_displayname = models.CharField(max_length=1024)
+    twitterProfile_id = models.CharField(max_length=1024)
+    twitterProfile_username = models.CharField(max_length=1024)
+    twitterProfile_displayname = models.CharField(max_length=1024)
+    subscriptions = models.ForeignKey(Subscription)
+    referral = models.CharField(max_length=1024)
+    refSrc = models.CharField(max_length=1024)
+    firstSeen = models.DateField(max_length=1024)
+    enableClassroom = models.BooleanField(default=False)
+    memberOf = models.CharField(max_length=1024)
+    isAffiliate = models.BooleanField(default=False)
+    isAWWAdmin = models.BooleanField(default=False)
+    accountType = models.CharField(max_length=1024, default='fun')
+    enableNewsletter = models.BooleanField(default=False)
+    lastLoginAt = models.DateField(max_length=1024)
+
+    def __str__(self):
+        return self.name
