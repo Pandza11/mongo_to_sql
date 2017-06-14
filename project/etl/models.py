e2cf2df330127165d76925d1c5ada071b876a38e
Pandza11 <ivan.pandzic@gmail.com>
2017-06-14 10:53:52 +0200

    Fields renamed according to the Python style guide

--- project/etl/models.py
+++ project/etl/models.py
@@ -9,17 +9,17 @@ from django.utils.timezone import datetime
 
 class Subscription(models.Model):
     """
-    Represents a subscription.
+    Represents a subscription by a User.
     """
     plan = models.CharField(max_length=1024, blank=False)
-    lastPayment = models.DateField(default=datetime.today)
-    createdAt = models.DateField(default=datetime.today)
-    paypalAgreementId = models.CharField(max_length=1024, blank=False)
-    isSuspended = models.BooleanField(default=False)
+    last_payment = models.DateField(default=datetime.today)
+    created_at = models.DateField(default=datetime.today)
+    paypal_agreement_id = models.CharField(max_length=1024, blank=False)
+    is_suspended = models.BooleanField(default=False)
     hosts = models.CharField(max_length=1024)
-    memberLimit = models.IntegerField(default=1)
+    member_limit = models.IntegerField(default=1)
     period = models.CharField(max_length=1024, default="monthly")
-    couponID = models.CharField(max_length=1024)
+    coupon_iD = models.CharField(max_length=1024)
 
     def __str__(self):
         return self.couponID
@@ -27,38 +27,38 @@ class Subscription(models.Model):
 
 class Account(models.Model):
     """
-    Represents a registered user.
+    Represents a registered User.
     """
     # what to do with index: { unique: true } in email?
     email = models.EmailField(max_length=254, blank=False)
     password = models.CharField(max_length=1024, blank=False)
-    createdAt = models.DateField(default=datetime.today)
+    created_at = models.DateField(default=datetime.today)
     name = models.CharField(max_length=1024)
     # what to do with index: { unique: true } in key?
     key = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     date = models.DateField(max_length=1024, default=datetime.today)
     # What should the default be?
-    defaultTTL = models.IntegerField(default=None)
-    googleProfile_id = models.CharField(max_length=1024)
-    googleProfile_username = models.CharField(max_length=1024)
-    googleProfile_displayname = models.CharField(max_length=1024)
-    facebookProfile_id = models.CharField(max_length=1024)
-    facebookProfile_username = models.CharField(max_length=1024)
-    facebookProfile_displayname = models.CharField(max_length=1024)
-    twitterProfile_id = models.CharField(max_length=1024)
-    twitterProfile_username = models.CharField(max_length=1024)
-    twitterProfile_displayname = models.CharField(max_length=1024)
+    default_ttl = models.IntegerField(default=None)
+    google_profile_id = models.CharField(max_length=1024)
+    google_profile_username = models.CharField(max_length=1024)
+    google_profile_displayname = models.CharField(max_length=1024)
+    facebook_profile_id = models.CharField(max_length=1024)
+    facebook_profile_username = models.CharField(max_length=1024)
+    facebook_profile_displayname = models.CharField(max_length=1024)
+    twitter_profile_id = models.CharField(max_length=1024)
+    twitter_profile_username = models.CharField(max_length=1024)
+    twitter_profile_displayname = models.CharField(max_length=1024)
     subscriptions = models.ForeignKey(Subscription)
     referral = models.CharField(max_length=1024)
-    refSrc = models.CharField(max_length=1024)
-    firstSeen = models.DateField()
-    enableClassroom = models.BooleanField(default=False)
-    memberOf = models.CharField(max_length=1024)
-    isAffiliate = models.BooleanField()
-    isAWWAdmin = models.BooleanField()
-    accountType = models.CharField(max_length=1024, default='fun')
-    enableNewsletter = models.BooleanField()
-    lastLoginAt = models.DateField(default=datetime.date(2017, 1, 1))
+    ref_src = models.CharField(max_length=1024)
+    first_seen = models.DateField()
+    enable_classroom = models.BooleanField(default=False)
+    member_of = models.CharField(max_length=1024)
+    is_affiliate = models.BooleanField()
+    is_aww_admin = models.BooleanField()
+    account_type = models.CharField(max_length=1024, default='fun')
+    enable_newsletter = models.BooleanField()
+    last_login_at = models.DateField(default=None)
 
     def __str__(self):
         return self.name
