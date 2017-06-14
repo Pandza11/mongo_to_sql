50d4df2acdf8e9ba51e0bfd5ae9092ad666b96bc
Pandza11 <ivan.pandzic@gmail.com>
2017-06-14 12:31:31 +0200

    Minor deletions in admin.py

--- project/etl/admin.py
+++ project/etl/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
-
 from .models import Subscription, Account
-# Register your models here.
 
 
 class SubscriptionInline(admin.StackedInline):
