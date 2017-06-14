ffc0e32848074061d9ecacc0feadcf21cef3acf0
Pandza11 <ivan.pandzic@gmail.com>
2017-06-14 16:35:37 +0200

    Removed dump and modified models

--- project/etl/models.py
+++ project/etl/models.py
@@ -1,5 +1,4 @@
 from __future__ import unicode_literals
-import uuid
 
 from django.db import models
 from django.utils.timezone import datetime
@@ -11,15 +10,12 @@ class Account(models.Model):
     """
     Represents a registered User.
     """
-    # what to do with index: { unique: true } in email?
-    email = models.EmailField(max_length=254, blank=False)
+    email = models.EmailField(max_length=254, blank=False, unique=True)
     created_at = models.DateField(default=datetime.today)
     name = models.CharField(max_length=1024)
-    # what to do with index: { unique: true } in key?
-    key = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    key = models.CharField(max_length=1024, unique=True)
     date = models.DateField(max_length=1024, default=datetime.today)
-    # What should the default be?
-    default_ttl = models.IntegerField(default=None)
+    default_ttl = models.IntegerField(default=None, unique=True)
     google_profile_id = models.CharField(max_length=1024)
     google_profile_username = models.CharField(max_length=1024)
     google_profile_displayname = models.CharField(max_length=1024)
@@ -53,7 +49,6 @@ class Subscription(models.Model):
     created_at = models.DateField(default=datetime.today)
     paypal_agreement_id = models.CharField(max_length=1024, blank=False)
     is_suspended = models.BooleanField(default=False)
-    hosts = models.CharField(max_length=1024)
     member_limit = models.IntegerField(default=1)
     period = models.CharField(max_length=1024, default="monthly")
     coupon_id = models.CharField(max_length=1024)
