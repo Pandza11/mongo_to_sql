5283bbf810877050239ba3c9d7b3b0cfbcc17565
Pandza11 <ivan.pandzic@gmail.com>
2017-07-19 01:06:37 +0200

    About to merge test.py and subs.py

--- project/subs.py
+++ project/subs.py
@@ -1,17 +1,15 @@
 import pymongo
 import sqlite3
 
-def unicode_to_bool(s):
-    if s == "True":
-        return True
-    else:
-        return False
 
-
-def change_keyname(oldkey,newkey):
-    """Renames dictionary key"""
-    if subscription.get(oldkey):
+def change_keyname(oldkey, newkey):
+    """
+    Renames dictionary key
+    """
+    try:
         subscription[newkey] = subscription.pop(oldkey)
+    except KeyError:
+        pass
 
 client = pymongo.MongoClient("localhost", 27017)
 db = client.awwapp
@@ -24,20 +22,18 @@ for account in db.accounts.find():
 
 for subscription in subscriptions_list:
     if len(subscription) > 0:
-        #subscription["created_at"] = subscription.pop("createdAt")
-        #subscription["paypal_agreement_id"] = subscription.pop("paypalAgreementId")
-        
-        '''
+        subscription.pop("_id", None)
 
-        if subscription.get("lastPayment"):
-            subscription["last_payment"] = subscription.pop("lastPayment")
-        '''
-
-        #change_keyname("memberLimit","member_limit")
+        change_keyname("memberLimit", "member_limit")
+        change_keyname("isSuspended", "is_suspended")
+        change_keyname("paypalAgreementId", "paypal_agreement_id")
+        change_keyname("lastPayment", "last_payment")
+        change_keyname("createdAt", "created_at")
 
         if subscription.get("couponId"):
             subscription["coupon_id"] = subscription.pop("couponId")
-            subscription["coupon_id"] = subscription["coupon_id"].encode("utf-8")
+            subscription["coupon_id"] = subscription["coupon_id"].encode(
+                "utf-8")
 
         if subscription.get("plan"):
             subscription["plan"] = subscription["plan"].encode("utf-8")
@@ -45,32 +41,32 @@ for subscription in subscriptions_list:
         if subscription.get("period"):
             subscription["period"] = subscription["period"].encode("utf-8")
 
-        #if subscription.get("hosts"):
-        #    hosts = subscription["hosts"]
-        #    x = ''.join(hosts).encode("utf-8")
-        #    print type(x)
-        #    subscription.pop("hosts", None)
-
-        subscription.pop("lastPayment", None)
-        subscription.pop("hosts", None)
-        subscription.pop("createdAt", None)
-        subscription.pop("paypalAgreementId", None)
-        subscription.pop("isSuspended", None)
-        subscription.pop("memberLimit", None)
-        subscription.pop("_id", None)
+        try:
+            hosts_list = subscription["hosts"]
+            if len(hosts_list) == 0:
+                subscription.pop("hosts", None)
+            elif len(hosts_list) == 1:
+                hosts_str = hosts_list[0].encode("utf-8")
+            elif len(hosts_list) > 1 and len(hosts_list[0]) > 0:
+                hosts_str = ', '.join(hosts_list)
+            subscription["hosts"] = hosts_str
+        except KeyError:
+            pass
 
 print(subscriptions_list[0])
 
 
 for value in subscriptions_list:
 
-    conn = sqlite3.connect('/home/ivan/pandzic/myproject/db.sqlite3')
+    conn = sqlite3.connect(
+        '/home/ivan/projects/project-aww/aww-etl/project/db.sqlite3')
     cursor = conn.cursor()
 
     columns = ', '.join(value.keys())
     placeholders = ', '.join('?' * len(value))
 
-    sql = 'INSERT INTO etl_subscription ({}) VALUES ({})'.format(columns, placeholders)
+    sql = 'INSERT INTO etl_subscription ({}) VALUES ({})'.format(
+        columns, placeholders)
     cursor.execute(sql, tuple(value.values()))
 
     conn.commit()
--- project/test.py
+++ project/test.py
@@ -1,19 +1,28 @@
 import pymongo
 import sqlite3
 
-def change_keyname(oldkey,newkey):
-    """Renames dictionary key"""
-    if account.get(oldkey):
+
+def change_keyname(oldkey, newkey):
+    """
+    Renames dictionary key
+    """
+    try:
         account[newkey] = account.pop(oldkey)
+    except KeyError:
+        pass
+
 
 def change_keyname_socialnetwork(socialnetwork):
-    """Renames dictionary key embedded within a dictionary"""
+    """
+    Renames dictionary key embedded within another dictionary
+    """
     social_list = ["id", "username", "displayName"]
     if account.get(socialnetwork+"Profile"):
         for item in social_list:
             if account.get(socialnetwork+"Profile").get(item):
-                account[socialnetwork+"_profile_"+item.lower()] = account.get(socialnetwork+"Profile").pop(item)
-    account.pop(socialnetwork+"Profile", None)       
+                account[socialnetwork+"_profile_"+item.lower()] = account.get(
+                    socialnetwork+"Profile").pop(item)
+    account.pop(socialnetwork+"Profile", None)
 
 client = pymongo.MongoClient("localhost", 27017)
 db = client.awwapp
@@ -27,43 +36,43 @@ for account in db.accounts.find():
 
 
 for account in db.accounts.find():
-    account.pop("_id", None) # Deleted according to instructions
-    account.pop("__v", None) # Deleted according to instructions
-    account.pop("password", None)  # Deleted according to instructions
-    account.pop("isCommboxAdmin", None) # Deleted here because unimportant (appears twice in db)
-    account.pop("googleId", None)   # Deleted here because it appears twice in the mongo database
-    account.pop("facebookId", None) # Deleted here because it appears twice in the mongo database
-    account.pop("twitterId", None)  # Deleted here because it appears twice in the mongo database
-    account.pop("subscriptions", None) # Requires further work
-    account.pop("enableNewsletter", None) # Function change_keyname doesn't work
+    deletion_list = ["_id", "__v", "password", "isCommboxAdmin", "googleId",
+                     "facebookId", "twitterId", "subscriptions",
+                     "enableNewsletter"]
+    for item in deletion_list:
+        account.pop(item, None)
 
-    change_keyname("accountType","account_type")
-    change_keyname("enableClassroom","enable_classroom")
-    change_keyname("refSrc","ref_src")
-    change_keyname("firstSeen","firstSeen")
-    change_keyname("isAffiliate","is_affiliate")
-    change_keyname("deletedAt","deleted_at")
-    change_keyname("memberOf","member_of")
-    change_keyname("defaultTTL","default_ttl")
-    change_keyname("lastLoginAt","last_login_at")
-    change_keyname("isAWWAdmin","is_aww_admin")
-    change_keyname("createdAt","created_at")
-    change_keyname_socialnetwork("google")
-    change_keyname_socialnetwork("facebook")
-    change_keyname_socialnetwork("twitter")
+    change_keyname_list = [["accountType", "account_type"],
+                           ["enableClassroom", "enable_classroom"],
+                           ["refSrc", "ref_src"],
+                           ["firstSeen", "firstSeen"],
+                           ["isAffiliate", "is_affiliate"],
+                           ["deletedAt", "deleted_at"],
+                           ["memberOf", "member_of"],
+                           ["defaultTTL", "default_ttl"],
+                           ["lastLoginAt", "last_login_at"],
+                           ["isAWWAdmin", "is_aww_admin"],
+                           ["createdAt", "created_at"]]
+    for item in change_keyname_list:
+        change_keyname(item[0], item[1])
+
+    change_keyname_socialnetwork_list = ["google", "facebook", "twitter"]
+    for item in deletion_list:
+        change_keyname_socialnetwork(item)
 
     accounts_list.append(account)
 
 
 for value in accounts_list:
-    conn = sqlite3.connect('/home/ivan/pandzic/myproject/db.sqlite3')
+    conn = sqlite3.connect(
+        '/home/ivan/projects/project-aww/aww-etl/project/db.sqlite3')
     cursor = conn.cursor()
 
     columns = ', '.join(value.keys())
     placeholders = ', '.join('?' * len(value))
 
-
-    sql = 'INSERT INTO etl_account ({}) VALUES ({})'.format(columns, placeholders)
+    sql = 'INSERT INTO etl_account ({}) VALUES ({})'.format(
+        columns, placeholders)
     cursor.execute(sql, tuple(value.values()))
 
     conn.commit()
