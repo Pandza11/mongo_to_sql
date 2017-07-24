7a6898e2439fa41a5c40ea5d3b2b700006f6a260
Pandza11 <ivan.pandzic@gmail.com>
2017-07-24 11:02:41 +0200

    Deleted unnecessary files

--- project/etl/management/commands/transfer.py
+++ project/etl/management/commands/transfer.py
@@ -5,7 +5,7 @@ os.path.dirname(os.path.dirname(__file__))
 
 
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
+    help = 'Transfers the contents of a mongo db to a sqlite db'
 
     def handle(self, *args, **options):
-        os.system('python test3.py')
+        os.system('python mongo_to_sqlite.py')
